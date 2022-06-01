cbe3f8e9df94b0082a40f4884ab9a7bf142862bd
prestonf2003 <33843946+prestonf2003@users.noreply.github.com>
2022-06-01 09:09:51 -0400

    Added back links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RedditServiceService } from './reddit-service.service';
 import {Reddit} from "./Reddit"
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,7 +23,7 @@ this.redditAPI.displayReddit()
   console.log(response);
   let name: string = "Hello"
   console.log(name);
-  console.log(this.posts);
+  console.dir(this.posts);
   this.posts.push(response);
 }
 )
